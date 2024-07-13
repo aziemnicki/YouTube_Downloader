3bed20d8ea3da64023ad3d698e09f985dcab2ce5
aziemnicki <a-ziemnicki@wp.pl>
2024-07-13 23:52:27 +0200

    Reformat

--- yt.py
+++ yt.py
@@ -3,19 +3,30 @@ import threading
 import pygame
 from pytube import YouTube
 import customtkinter
-from tkinter import filedialog, Tk
+from tkinter import filedialog
 import pygame.mixer as mix
-from moviepy.editor import *
+from moviepy.editor import VideoFileClip
 from ctypes import cast, POINTER
 from comtypes import CLSCTX_ALL
 from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import sys
+import os
 
+
+def resource_path(relative_path):
+    """Get absolute path to resource, works for dev and for PyInstaller"""
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS2
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
 
 
 def get_system_volume():
     devices = AudioUtilities.GetSpeakers()
-    interface = devices.Activate(
-        IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
     volume = cast(interface, POINTER(IAudioEndpointVolume))
     current_volume = volume.GetMasterVolumeLevelScalar()
     return current_volume
@@ -34,9 +45,7 @@ class App(customtkinter.CTk):
         self.button_play = False
         self.song_title = None
         self.timestamp = 0
-        self.queue=[]
-
-
+        self.queue = []
 
         self.title("Music Downloader")
         self.geometry("900x450")
@@ -45,78 +54,132 @@ class App(customtkinter.CTk):
         self.grid_rowconfigure(0, weight=1)
         self.grid_rowconfigure(1, weight=0)
 
-        self.frame = customtkinter.CTkFrame(self, width=500, height=300, corner_radius=10)
+        self.frame = customtkinter.CTkFrame(
+            self, width=500, height=300, corner_radius=10
+        )
         self.frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        # self.frame.pack(pady=20, padx=10, fill="both", expand=True)
 
-        self.label = customtkinter.CTkLabel(self.frame,  text="Wklej link do piosenki na YouTube", font=("Roboto", 24))
-        self.label.grid(row=0, column=0, pady=6, padx=10, sticky='nsew')
-        # self.label.pack(pady=12, padx=10)
-        self.entry1 = customtkinter.CTkEntry(master=self.frame, placeholder_text="Miejsce na link", width=400, height=50)
+        self.label = customtkinter.CTkLabel(
+            self.frame, text="Wklej link do piosenki na YouTube", font=("Roboto", 24)
+        )
+        self.label.grid(row=0, column=0, pady=6, padx=10, sticky="nsew")
+        self.entry1 = customtkinter.CTkEntry(
+            master=self.frame, placeholder_text="Miejsce na link", width=400, height=50
+        )
         self.entry1.grid(row=1, column=0, padx=30, pady=0)
-        #self.entry1.pack(pady=2, padx=10)
-        self.label2 = customtkinter.CTkLabel(master=self.frame, text='Wybierz folder do pobrania',
-                                        font=("Roboto", 12))
-        self.label2.grid(row=0, column=1, padx=10, pady=(10,0), sticky='w')
-        self.label3 = customtkinter.CTkLabel(master=self.frame,
-                                             text='(domyślnie "Pobrane")',
-                                             font=("Roboto", 12))
-        self.label3.grid(row=0, column=1, padx=10, pady=(50, 0), sticky='w')
-        # label2.pack(pady=2, padx=10)
-        self.button1 = customtkinter.CTkButton(master=self.frame, text="Otwórz folder", command=self.foldery)
-        self.button1.grid(row=1, column=1, padx=10, pady=12, sticky='w')
-        # self.button1.pack(pady=12, padx=10, side="top")
+        self.label2 = customtkinter.CTkLabel(
+            master=self.frame, text="Wybierz folder do pobrania", font=("Roboto", 12)
+        )
+        self.label2.grid(row=0, column=1, padx=10, pady=(10, 0), sticky="w")
+        self.label3 = customtkinter.CTkLabel(
+            master=self.frame, text='(domyślnie "Pobrane")', font=("Roboto", 12)
+        )
+        self.label3.grid(row=0, column=1, padx=10, pady=(50, 0), sticky="w")
+        self.button1 = customtkinter.CTkButton(
+            master=self.frame, text="Otwórz folder", command=self.foldery
+        )
+        self.button1.grid(row=1, column=1, padx=10, pady=12, sticky="w")
 
-        self.button2 = customtkinter.CTkButton(master=self.frame, text="Pobierz", command=self.download, width=100)
-        self.button2.grid(row=2, column=0, padx=150, pady=12, sticky='nsew')
-        # self.button2.pack(pady=12, padx=10, side="bottom")
-        self.checkbox_1 = customtkinter.CTkCheckBox(master=self.frame, state="disabled", text="")
+        self.button2 = customtkinter.CTkButton(
+            master=self.frame, text="Pobierz", command=self.download, width=100
+        )
+        self.button2.grid(row=2, column=0, padx=150, pady=12, sticky="nsew")
+        self.checkbox_1 = customtkinter.CTkCheckBox(
+            master=self.frame, state="disabled", text=""
+        )
         self.checkbox_1.grid(row=2, column=1, pady=(20, 0), padx=20, sticky="n")
 
-        self.scroll = customtkinter.CTkScrollableFrame(self, label_text='Playlista', height=150, width=200, fg_color='#d2f7f5')
-        self.scroll.grid(row=0, column=0, padx=10, pady=(10,0), sticky='e')
+        self.scroll = customtkinter.CTkScrollableFrame(
+            self, label_text="Playlista", height=150, width=200, fg_color="#d2f7f5"
+        )
+        self.scroll.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="e")
         self.scroll.grid_columnconfigure(0, weight=1)
 
-        self.label_frame = customtkinter.CTkFrame(self, )
+        self.label_frame = customtkinter.CTkFrame(
+            self,
+        )
         self.label_frame.grid(row=1, column=0, padx=15, pady=(10, 0), sticky="ew")
-        self.label4 = customtkinter.CTkLabel(self.label_frame,
-                                             text="Odtwarzacz muzyki, wklej link aby odsłuchać i sprawdzić",
-                                             font=("Roboto", 16))
-        self.label4.grid(row=0, column=0, padx=(200,0), pady=5, sticky='e')
-
+        self.label4 = customtkinter.CTkLabel(
+            self.label_frame,
+            text="Odtwarzacz muzyki, wklej link aby odsłuchać i sprawdzić",
+            font=("Roboto", 16),
+        )
+        self.label4.grid(row=0, column=0, padx=(200, 0), pady=5, sticky="e")
 
         self.player_frame = Frame(self, width=500, corner_radius=20)
         self.player_frame.configure(fg_color="#cce6d3", height=50)
-        self.player_frame.grid(row=2, column=0,padx=10, pady=10, sticky='s')
+        self.player_frame.grid(row=2, column=0, padx=10, pady=10, sticky="s")
 
-        self.button3 = customtkinter.CTkButton(master=self.player_frame, text='<<', text_color="#000000", width=100,
-                                               font=customtkinter.CTkFont(size=12, weight="bold"), command=self.reverse)
-        self.button3.grid(row=0, column=0, padx=(20, 10), pady=50, sticky='nswe')
-        self.button4 = customtkinter.CTkButton(master=self.player_frame, text='| |',text_color="#000000", width=100,
-                                               font=customtkinter.CTkFont(size=12, weight="bold"), command=self.stop)
-        self.button4.grid(row=0, column=1, padx=10, pady=50, sticky='nswe')
-        self.button5 = customtkinter.CTkButton(master=self.player_frame, text='PLAY',text_color="#000000", width=100,
-                                               font=customtkinter.CTkFont(size=12, weight="bold"), command=self.play)
-        self.button5.grid(row=0, column=2, padx=10, pady=50, sticky='nswe')
-        self.button6 = customtkinter.CTkButton(master=self.player_frame, text='>>',text_color="#000000", width=100,
-                                               font=customtkinter.CTkFont(size=12, weight="bold"), command=self.next)
-        self.button6.grid(row=0, column=3, padx=10, pady=50, sticky='nswe')
-        self.button7 = customtkinter.CTkButton(master=self.player_frame, text='NEXT>>', text_color="#000000", width=100,
-                                               font=customtkinter.CTkFont(size=12, weight="bold"), command=self.next_song)
-        self.button7.grid(row=0, column=4, padx=(10, 20), pady=50, sticky='nswe')
+        self.button3 = customtkinter.CTkButton(
+            master=self.player_frame,
+            text="<<",
+            text_color="#000000",
+            width=100,
+            font=customtkinter.CTkFont(size=12, weight="bold"),
+            command=self.reverse,
+        )
+        self.button3.grid(row=0, column=0, padx=(20, 10), pady=50, sticky="nswe")
+        self.button4 = customtkinter.CTkButton(
+            master=self.player_frame,
+            text="| |",
+            text_color="#000000",
+            width=100,
+            font=customtkinter.CTkFont(size=12, weight="bold"),
+            command=self.stop,
+        )
+        self.button4.grid(row=0, column=1, padx=10, pady=50, sticky="nswe")
+        self.button5 = customtkinter.CTkButton(
+            master=self.player_frame,
+            text="PLAY",
+            text_color="#000000",
+            width=100,
+            font=customtkinter.CTkFont(size=12, weight="bold"),
+            command=self.play,
+        )
+        self.button5.grid(row=0, column=2, padx=10, pady=50, sticky="nswe")
+        self.button6 = customtkinter.CTkButton(
+            master=self.player_frame,
+            text=">>",
+            text_color="#000000",
+            width=100,
+            font=customtkinter.CTkFont(size=12, weight="bold"),
+            command=self.next,
+        )
+        self.button6.grid(row=0, column=3, padx=10, pady=50, sticky="nswe")
+        self.button7 = customtkinter.CTkButton(
+            master=self.player_frame,
+            text="NEXT>>",
+            text_color="#000000",
+            width=100,
+            font=customtkinter.CTkFont(size=12, weight="bold"),
+            command=self.next_song,
+        )
+        self.button7.grid(row=0, column=4, padx=(10, 20), pady=50, sticky="nswe")
 
-        self.slider_progressbar_frame = customtkinter.CTkFrame(self, fg_color="transparent", width=50, height=150)
-        self.slider_progressbar_frame.grid(row=2, column=0, padx=(20, 0), pady=(20, 0), sticky="e")
-        self.slider = customtkinter.CTkSlider(self.player_frame, orientation="vertical", number_of_steps=100,
-                                            from_=0, to=100, height=100, command=self.volume)
-        self.slider.grid(row=0, column=5, rowspan=5, padx=(10, 40), pady=(10, 10), sticky="ns")
-        self.slider.set(volume*100)
+        self.slider_progressbar_frame = customtkinter.CTkFrame(
+            self, fg_color="transparent", width=50, height=150
+        )
+        self.slider_progressbar_frame.grid(
+            row=2, column=0, padx=(20, 0), pady=(20, 0), sticky="e"
+        )
+        self.slider = customtkinter.CTkSlider(
+            self.player_frame,
+            orientation="vertical",
+            number_of_steps=100,
+            from_=0,
+            to=100,
+            height=100,
+            command=self.volume,
+        )
+        self.slider.grid(
+            row=0, column=5, rowspan=5, padx=(10, 40), pady=(10, 10), sticky="ns"
+        )
+        self.slider.set(volume * 100)
 
     def volume(self, value):
         print(self.slider.get())
-        self.new_val = value/100.0
+        self.new_val = value / 100.0
         mix.music.set_volume(self.new_val)
-
 
     def reverse(self):
         if mix.music.get_busy():
@@ -132,14 +195,11 @@ class App(customtkinter.CTk):
             mix.music.unpause()
             self.button4.configure(fg_color="#2cc985")
 
-
     def play(self):
-        os.chdir(os.path.join(os.getcwd(), 'temp_audio'))
+        os.chdir(os.path.join(os.getcwd(), "temp_audio"))
         self.selected_path = os.getcwd()
         self.download()
         files = os.listdir(self.selected_path)
-        print(files)
-        # print(self.song_title)
         if self.button_play:
             mix.music.queue(files[0])
             print("Dodano do kolejki")
@@ -148,25 +208,44 @@ class App(customtkinter.CTk):
             self.labels = []
             for i in range(len(self.queue)):
                 label = customtkinter.CTkLabel(master=self.scroll, text="")
-                label.grid(row=i, column=0, padx=5, pady=5, )
-                label = customtkinter.CTkLabel(master=self.scroll, text=f"{self.queue[i]}")
-                label.grid(row=i, column=0, padx=5, pady=5, )
+                label.grid(
+                    row=i,
+                    column=0,
+                    padx=5,
+                    pady=5,
+                )
+                label = customtkinter.CTkLabel(
+                    master=self.scroll, text=f"{self.queue[i]}"
+                )
+                label.grid(
+                    row=i,
+                    column=0,
+                    padx=5,
+                    pady=5,
+                )
         else:
             self.button_play = True
-            for f in range(len( files)):
+            for f in range(len(files)):
                 if files[f] == self.song_title:
                     print(self.song_title)
                     mix.music.load(os.path.join(self.selected_path, self.song_title))
                     mix.music.play()
-            label = customtkinter.CTkLabel(master=self.scroll, text=f"{self.song_title}")
-            label.grid(row=0, column=0, padx=5, pady=5, )
+            label = customtkinter.CTkLabel(
+                master=self.scroll, text=f"{self.song_title}"
+            )
+            label.grid(
+                row=0,
+                column=0,
+                padx=5,
+                pady=5,
+            )
 
         os.chdir(os.path.dirname(os.getcwd()))
         print(os.getcwd())
 
     def next(self):
         if mix.music.get_busy():
-            self.timestamp += (mix.music.get_pos()/1000)+10
+            self.timestamp += (mix.music.get_pos() / 1000) + 10
             print(self.timestamp)
             mix.music.rewind()
             mix.music.set_pos(self.timestamp)
@@ -175,7 +254,6 @@ class App(customtkinter.CTk):
         if mix.music.get_busy():
             if self.queue:
                 next_song = self.queue[0]
-                # Pobierz następną piosenkę z kolejki
                 try:
                     mix.music.load(os.path.join(self.selected_path, next_song))
                     mix.music.play()
@@ -183,35 +261,34 @@ class App(customtkinter.CTk):
                     print(e)
                 self.queue.pop(0)
 
-
     def foldery(self):
-        if self.selected == False:
-            self.selected_path = filedialog.askdirectory(initialdir=main_path, title="Wybierz folder")
+        if not self.selected:
+            self.selected_path = filedialog.askdirectory(
+                initialdir=main_path, title="Wybierz folder"
+            )
             print(self.selected_path)
         else:
             self.selected_path = main_path
         return self.selected_path
 
-
     def download(self):
         link = app.entry1.get()
         if self.selected_path is None:
             self.selected_path = main_path
-        # elif self.button_play:
-        #     print(os.getcwd())
-        #     self.selected_path = os.getcwd()
 
         if link != "":
-            # print("test")
             yt = YouTube(link)
-            song = yt.streams.filter(file_extension='mp4', resolution=yt.streams.get_highest_resolution()).first()
+
+            song = yt.streams.filter(
+                file_extension="mp4", resolution=yt.streams.get_highest_resolution()
+            ).first()
+            print(song)
             # print("Tytuł: ", yt.title)
             # print("Długość: ", yt.length)
             file = song.download(output_path=self.selected_path, skip_existing=True)
-            # mp3_file = 'audio.mp3'
             base, ext = os.path.splitext(file)
-            new_file = base + '.mp3'
-            self.song_title = yt.title + '.mp3'
+            new_file = base + ".mp3"
+            self.song_title = yt.title + ".mp3"
             try:
                 if new_file not in os.listdir(os.getcwd()):
                     video = VideoFileClip(file)
@@ -222,21 +299,13 @@ class App(customtkinter.CTk):
                     os.remove(file)
             except IOError as e:
                 print(e)
-            # print(new_file)
-            # self.convert(file, "audio.mp3")
 
-            # copy2(new_file, os.path.join(os.getcwd(), 'temp_audio'))
-            print('Pobrano ', yt.title)
+            print("Pobrano ", yt.title)
             self.checkbox_1.configure(text="Pobrano", font=("Roboto", 12))
             self.checkbox_1.select()
             app.entry1.delete(0, "end")
             thread = threading.Thread(target=self.wait)
             thread.start()
-
-
-            # yd=yt.streams.get_highest_resolution()
-            # yd.download('.Download')
-            # self.download_and_convert(link, self.selected_path)
 
     def wait(self):
         time.sleep(3)
@@ -248,7 +317,6 @@ if __name__ == "__main__":
     mix.init()
     folder = os.path.expanduser("~")
     main_path = os.path.join(folder, "Downloads")
-    # temp_path = os.path('temp_audio')
 
     customtkinter.set_appearance_mode("system")
     customtkinter.set_default_color_theme("green")
@@ -256,11 +324,11 @@ if __name__ == "__main__":
     # sound = mix.music.load('audio.mp3')
     # mix.music.play()
     volume = get_system_volume()
-    print('good')
+    print("good")
     app = App()
     app.mainloop()
     mix.music.unload()
-    os.chdir(os.path.join(os.getcwd(), 'temp_audio'))
+    os.chdir(os.path.join(os.getcwd(), "temp_audio"))
     path = os.getcwd()
     files = os.listdir(path)
     for f in files:
